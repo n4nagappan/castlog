8cd52492c5e1ddf3ecd52fc09b80e60d09cb32d8
Nagappan Nachiappan <n4nagappan@gmail.com>
2015-03-18 01:12:37 -0400

    Added heartbeats to keep the connection alive

--- castlog.js
+++ castlog.js
@@ -1,5 +1,8 @@
 #! /usr/bin/env node
 
+
+// This is a simple command line tool to connect to the server and stream logs from the piped input stream
+
 var WebSocket = require('ws');
 var ws = new WebSocket('ws://107.170.209.244/wss');
 //var ws = new WebSocket('ws://localhost:9999');
@@ -9,12 +12,20 @@ ws.on('open', function(){
     process.stdin.on('readable', function(){
         var data = process.stdin.read();
         if( data != null){
-            ws.send(data);
+            var obj = { event : "message" , data : data }; 
+            ws.send(JSON.stringify(obj));
         }
     });
+    // send heart beats to prevent the socket connection from closing down
+    setInterval( heartbeat, 1000);
 });
 
 
 ws.on('message', function( data, flags ){
     console.log("Access your log stream at : " + data);
 });
+
+function heartbeat(){
+    var obj = { event : "heartbeat" };
+    ws.send(JSON.stringify(obj));
+}
